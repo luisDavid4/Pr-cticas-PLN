910f8e09292b7aab10b97c8b5b93950a57feac8d
Luis David <ldavid_panama2@hotmail.com>
2019-08-26 22:31:50 -0500

    Resultado

--- 2_JuegoDeLosNombres/JuegoDeLosNombres.py
+++ 2_JuegoDeLosNombres/JuegoDeLosNombres.py
@@ -9,8 +9,8 @@ def clear(): return os.system("cls")
 
 
 class Agente:
-    def __init__(self, m):
-        self.m = m  # el número total de objetos en el contexto
+    def __init__(self, m):  # m=el número total de objetos en el contexto
+        self.nombresObjetos = [[] for i in range(m)]
 
     def nombrar(self):
         nombre = ""
@@ -22,16 +22,110 @@ class Agente:
             nombre += f"{random.choice(consonantes)}{random.choice(vocales)}"
         return nombre
 
+    def enunciar(self, k, oyente):  # k=número de objeto, a=agente oyente
+        exito = False
+        nombreCorto = ""
+        for nombre in self.nombresObjetos[k]:
+            if nombreCorto == "" or len(nombre) < len(nombreCorto):
+                nombreCorto = nombre
+        if nombreCorto == "":
+            nombreCorto = self.nombrar()
+            self.nombresObjetos[k].append(nombreCorto)
+        # Comunicar el nombre corto al oyente:
+        if nombreCorto in oyente.nombresObjetos[k]:
+            exito = True
+            self.nombresObjetos[k] = [nombreCorto]
+            oyente.nombresObjetos[k] = [nombreCorto]
+        else:
+            oyente.nombresObjetos[k].append(nombreCorto)
+        return exito
+
 
 class Objeto:
-    def __init__(self):
-        nombresCandidatos = []
+    nombre = ""
+
+
+def imprimirMetricas(agentes, t):
+    soloUnNombrePorObj = True
+    numObjetos = len(agentes[0].nombresObjetos)
+    numNombresConocidos = 0
+    numNombresCadaObjeto = ""
+    longMediaNombres = 0  # en número de letras
+    todosNombres = []
+    listaNombresCadaObjeto = [[] for i in range(numObjetos)]
+    # Obtención de todos los nombres
+    for agente in agentes:
+        for i in range(numObjetos):
+            for nombreObjeto in agente.nombresObjetos[i]:
+                listaNombresCadaObjeto[i].append(nombreObjeto)
+                todosNombres.append(nombreObjeto)
+    # Preparación de mensajes:
+    for i in range(numObjetos):
+        listaNombresCadaObjeto[i] = list(dict.fromkeys(
+            listaNombresCadaObjeto[i]))  # quitar duplicados
+        cantidadNombres = len(listaNombresCadaObjeto[i])
+        numNombresCadaObjeto += f"Obj{i+1}={cantidadNombres} "
+        if cantidadNombres != 1:
+            soloUnNombrePorObj = False
+    todosNombres = list(dict.fromkeys(todosNombres))  # quitar duplicados
+    numNombresConocidos = len(todosNombres)
+    sumaLetras = 0
+    for nombre in todosNombres:
+        sumaLetras += len(nombre)
+    longMediaNombres = sumaLetras/numNombresConocidos
+    print(f"\nNúmero total de nombres conocidos: {numNombresConocidos}")
+    print(f"Número total de nombres para cada objeto: {numNombresCadaObjeto}")
+    print(
+        f"Longitud promedio de todos los nombres: {'%.1f'%(longMediaNombres)} letras")
+    print(f"Número de iteración: {t}")
+    return soloUnNombrePorObj
+
+
+def esEstable(agentes):
+    estable = True
+    numObjetos = len(agentes[0].nombresObjetos)
+    for i in range(numObjetos):
+        for agente in agentes:
+            # verificar que cada agente tenga solo 1 nombre para cada objeto
+            if len(agente.nombresObjetos[i]) != 1\
+                    or len(agentes[0].nombresObjetos[i]) != 1:
+                estable = False
+            elif agente.nombresObjetos[i][0] != agentes[0].nombresObjetos[i][0]:
+                estable = False
+    return estable
 
 
 def main():
-    tiempo = 0
-    hablante = Agente(1)
-    return hablante.nombrar()
+    mensaje = ""
+    n = int(input(f"Ingrese el número de agentes (n): "))
+    m = int(input(f"Ingrese el número de objetos (m): "))
+    max_t = int(input(f"Ingrese el número de iteraciones máximas (max_t): "))
+    t = 0  # número de iteración
+    listaAgentes = []
+    listaObjetos = []
+    for i in range(n):
+        listaAgentes.append(Agente(m))
+    for i in range(m):
+        listaObjetos.append(Objeto())
+    vocabularioEstable = False
+    while t < max_t and not vocabularioEstable:
+        t += 1
+        hablante = random.choice(listaAgentes)
+        oyente = random.choice(listaAgentes)
+        while hablante == oyente:
+            oyente = random.choice(listaAgentes)
+        objAleatorio = random.randrange(0, m)  # valores entre [x,y)
+        hablante.enunciar(objAleatorio, oyente)
+        if imprimirMetricas(listaAgentes, t):
+            vocabularioEstable = esEstable(listaAgentes)
+    if vocabularioEstable:
+        mensaje = "\n\tNombre final de cada objeto:\n"
+        for i in range(m):
+            listaObjetos[i].nombre = listaAgentes[0].nombresObjetos[i][0]
+            mensaje += f"Obj{i+1} = {listaObjetos[i].nombre}\n"
+    else:
+        mensaje = f"\n\tEn la iteración máxima no se obtuvo el vocabulario estable.\n"
+    return mensaje
 
 
 if __name__ == "__main__":
--- 2_JuegoDeLosNombres/README.md
+++ 2_JuegoDeLosNombres/README.md
@@ -1,10 +1,110 @@
 # JuegoDeLosNombres
+
 Alumno: Luis David Panamá Miranda
 Clase: PLN, MSICU.
 
-Este es el archivo README de la práctica 2. El cual incluye:
+Este es el archivo README de la práctica 2. La cual incluye:
 
 1. El juego de nombres, definida en el archivo 02.practica.pdf
+2. Resultado parcial (en este archivo README) y completo (en el archivo resultadoCompleto.txt)
+
+### Resultado
+
+A continuación se presenta el resultado de las primeras 10 y las últimas 5 iteraciones del script (n=14, m=7 y max_t=1000).
+Nota: Adicionalmente el resultado completo se encuentra en el archivo resultadoCompleto.txt
+
+Ingrese el número de agentes (n): 14
+Ingrese el número de objetos (m): 7
+Ingrese el número de iteraciones máximas (max_t): 1000
+
+Número total de nombres conocidos: 1
+Número total de nombres para cada objeto: Obj1=0 Obj2=0 Obj3=0 Obj4=0 Obj5=1 Obj6=0 Obj7=0
+Longitud promedio de todos los nombres: 6.0 letras
+Número de iteración: 1
+
+Número total de nombres conocidos: 2
+Número total de nombres para cada objeto: Obj1=0 Obj2=0 Obj3=0 Obj4=0 Obj5=1 Obj6=0 Obj7=1
+Longitud promedio de todos los nombres: 9.0 letras
+Número de iteración: 2
+
+Número total de nombres conocidos: 3
+Número total de nombres para cada objeto: Obj1=1 Obj2=0 Obj3=0 Obj4=0 Obj5=1 Obj6=0 Obj7=1
+Longitud promedio de todos los nombres: 10.7 letras
+Número de iteración: 3
+
+Número total de nombres conocidos: 4
+Número total de nombres para cada objeto: Obj1=2 Obj2=0 Obj3=0 Obj4=0 Obj5=1 Obj6=0 Obj7=1
+Longitud promedio de todos los nombres: 11.5 letras
+Número de iteración: 4
+
+Número total de nombres conocidos: 5
+Número total de nombres para cada objeto: Obj1=2 Obj2=0 Obj3=0 Obj4=0 Obj5=1 Obj6=0 Obj7=2
+Longitud promedio de todos los nombres: 10.4 letras
+Número de iteración: 5
+
+Número total de nombres conocidos: 6
+Número total de nombres para cada objeto: Obj1=2 Obj2=0 Obj3=0 Obj4=1 Obj5=1 Obj6=0 Obj7=2
+Longitud promedio de todos los nombres: 9.7 letras
+Número de iteración: 6
+
+Número total de nombres conocidos: 6
+Número total de nombres para cada objeto: Obj1=2 Obj2=0 Obj3=0 Obj4=1 Obj5=1 Obj6=0 Obj7=2
+Longitud promedio de todos los nombres: 9.7 letras
+Número de iteración: 7
+
+Número total de nombres conocidos: 7
+Número total de nombres para cada objeto: Obj1=2 Obj2=0 Obj3=0 Obj4=1 Obj5=1 Obj6=1 Obj7=2
+Longitud promedio de todos los nombres: 9.7 letras
+Número de iteración: 8
+
+Número total de nombres conocidos: 8
+Número total de nombres para cada objeto: Obj1=2 Obj2=0 Obj3=0 Obj4=1 Obj5=2 Obj6=1 Obj7=2
+Longitud promedio de todos los nombres: 10.0 letras
+Número de iteración: 9
+
+Número total de nombres conocidos: 9
+Número total de nombres para cada objeto: Obj1=2 Obj2=0 Obj3=1 Obj4=1 Obj5=2 Obj6=1 Obj7=2
+Longitud promedio de todos los nombres: 10.2 letras
+Número de iteración: 10
+
+---
+
+## Últimas 5 iteraciones del script:
+
+Número total de nombres conocidos: 8
+Número total de nombres para cada objeto: Obj1=1 Obj2=1 Obj3=1 Obj4=1 Obj5=1 Obj6=2 Obj7=1
+Longitud promedio de todos los nombres: 4.8 letras
+Número de iteración: 943
+
+Número total de nombres conocidos: 8
+Número total de nombres para cada objeto: Obj1=1 Obj2=1 Obj3=1 Obj4=1 Obj5=1 Obj6=2 Obj7=1
+Longitud promedio de todos los nombres: 4.8 letras
+Número de iteración: 944
+
+Número total de nombres conocidos: 8
+Número total de nombres para cada objeto: Obj1=1 Obj2=1 Obj3=1 Obj4=1 Obj5=1 Obj6=2 Obj7=1
+Longitud promedio de todos los nombres: 4.8 letras
+Número de iteración: 945
+
+Número total de nombres conocidos: 8
+Número total de nombres para cada objeto: Obj1=1 Obj2=1 Obj3=1 Obj4=1 Obj5=1 Obj6=2 Obj7=1
+Longitud promedio de todos los nombres: 4.8 letras
+Número de iteración: 946
+
+Número total de nombres conocidos: 7
+Número total de nombres para cada objeto: Obj1=1 Obj2=1 Obj3=1 Obj4=1 Obj5=1 Obj6=1 Obj7=1
+Longitud promedio de todos los nombres: 3.4 letras
+Número de iteración: 947
+
+        Nombre final de cada objeto:
+
+Obj1 = ve
+Obj2 = lo
+Obj3 = tuwo
+Obj4 = joñeso
+Obj5 = kiruqa
+Obj6 = ju
+Obj7 = do
 
 ### Prerequisitos
 
